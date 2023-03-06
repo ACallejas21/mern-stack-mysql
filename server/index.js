f7a63b1e88feffb0b2bb19662ae93d28cc200c97
Acalleas21 <akdiel.callejas21@gmail.com>
2023-03-06 13:29:58 -0600

    importando las rutas y agregandolas a la app

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { PORT } from "./config.js";
 
+//*Rutas
+import indexRoutes from "./routes/index.routes.js";
+
 
 const app = express()
 
+app.use(indexRoutes)
+
 app.listen(PORT)
 
-console.log('server listening on port 3000');
+console.log(`server listening on port ${PORT}`);
