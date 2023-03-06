9a46a7544bbebd2df5cce670810149093a513249
Acalleas21 <akdiel.callejas21@gmail.com>
2023-03-06 13:29:32 -0600

    configuración para la conexion a la bd

--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
-import {  createPool} from "mysql2";
+import {  createPool} from "mysql2/promise";
 
 export const pool = createPool({
     host: 'localhost',
     port: '3306',
-    user: 'root',
+    user: 'shadow',
     password: '1234',
     database: 'tasksdb'
 })
