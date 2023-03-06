c7839efe92c5ef18a42d38a4e7445e13b7cb7a4e
Acalleas21 <akdiel.callejas21@gmail.com>
2023-03-06 13:56:23 -0600

    creando controladpres iniciales

---
+++ server/controllers/task.controllers.js
@@ -0,0 +1,19 @@
+export const getTask = (req, res) => {
+    res.send('obteniendo Tarea')
+}
+
+export const getTasks = (req, res) => {
+    res.send('obteniendo Tareas')
+}
+
+export const createTask = (req, res) => {
+    res.send('creando Tareas')
+}
+
+export const updateTask = (req, res) => {
+    res.send('actualizando Tareas')
+}
+
+export const deleteTask = (req, res) => {
+    res.send('eliminando Tareas')
+}
