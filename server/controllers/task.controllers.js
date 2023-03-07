45498ac71fd5ac424a2c7f749a461a00e40aa18e
Acalleas21 <akdiel.callejas21@gmail.com>
2023-03-07 13:47:23 -0600

    create task endpoint

--- server/controllers/task.controllers.js
+++ server/controllers/task.controllers.js
@@ -1,19 +1,32 @@
+import { pool } from "../db.js";
+
 export const getTask = (req, res) => {
-    res.send('obteniendo Tarea')
-}
+  res.send("obteniendo Tarea");
+};
 
 export const getTasks = (req, res) => {
-    res.send('obteniendo Tareas')
-}
+  res.send("obteniendo Tareas");
+};
 
-export const createTask = (req, res) => {
-    res.send('creando Tareas')
-}
+export const createTask = async (req, res) => {
+  const { title, description } = req.body;
+  const [result] = await pool.query(
+    "INSERT INTO tasks (title, description) VALUES (? , ?)",
+    [title, description]
+  );
+  console.log(result);
+  res.json({
+    id: result.insertId,
+    title,
+    description,
+  });
+  res.send("creando Tareas");
+};
 
 export const updateTask = (req, res) => {
-    res.send('actualizando Tareas')
-}
+  res.send("actualizando Tareas");
+};
 
 export const deleteTask = (req, res) => {
-    res.send('eliminando Tareas')
-}
+  res.send("eliminando Tareas");
+};
