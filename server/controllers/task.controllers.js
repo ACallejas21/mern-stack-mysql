42f493c515711199dd9f1117bbdf771025f4e476
Acalleas21 <akdiel.callejas21@gmail.com>
2023-03-07 14:07:15 -0600

    GET TASK ENDPOINT

--- server/controllers/task.controllers.js
+++ server/controllers/task.controllers.js
@@ -1,13 +1,24 @@
 import { pool } from "../db.js";
 
-export const getTask = (req, res) => {
-  res.send("obteniendo Tarea");
+//* ----------------- GET TASK BY ID -------------------------
+export const getTask = async (req, res) => {
+  const [result] = await pool.query("SELECT * FROM tasks WHERE id = ? ", [
+    req.params.id,
+  ]);
+
+  if (result.length === 0)
+    return res.status(404).json({mensaje: "TASK NOT FOUND"})
+
+  res.json(result[0]);
 };
 
-export const getTasks = (req, res) => {
-  res.send("obteniendo Tareas");
+//* ----------------- GET ALL TASKS -------------------------
+export const getTasks = async (req, res) => {
+  const [result] = await pool.query("SELECT * FROM tasks ORDER BY ASC");
+  res.json(result);
 };
 
+//* ----------------- CREATE TASK -------------------------
 export const createTask = async (req, res) => {
   const { title, description } = req.body;
   const [result] = await pool.query(
