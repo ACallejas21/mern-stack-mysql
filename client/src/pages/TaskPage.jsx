5da6b7ef56e4af5b45c98057d684600881bb5c91
Acalleas21 <akdiel.callejas21@gmail.com>
2023-03-08 14:43:32 -0600

    getTasks

--- client/src/pages/TaskPage.jsx
+++ client/src/pages/TaskPage.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useEffect, useState} from "react";
+import { GetTaskRequest } from "../api/task.api";
+import TaskCard from "../components/TaskCard";
 
 function TaskPage() {
-  return (
-    <div>TaskPage</div>
-  )
+
+  const [task, setTask] = useState([])
+
+  useEffect(() => {
+    async function LoadTask() {
+      const response = await GetTaskRequest();
+      console.log(response);
+      setTask(response.data);
+    }
+    LoadTask()
+  }, []);
+
+  return (<div>
+    <h1>Tasks</h1>
+    {task.map(task => (
+      <TaskCard task={task} key={task.id}></TaskCard>
+    ))}
+  </div>);
 }
 
-export default TaskPage
+export default TaskPage;
