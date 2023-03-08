36db456871ae7b849c9dbb4186507690e37685d2
Acalleas21 <akdiel.callejas21@gmail.com>
2023-03-08 10:07:15 -0600

    ERROR HANDLER

--- server/controllers/task.controllers.js
+++ server/controllers/task.controllers.js
@@ -2,57 +2,75 @@ import { pool } from "../db.js";
 
 //* ----------------- GET TASK BY ID -------------------------
 export const getTask = async (req, res) => {
-  const [result] = await pool.query("SELECT * FROM tasks WHERE id = ? ", [
-    req.params.id,
-  ]);
+  try {
+    const [result] = await pool.query("SELECT * FROM tasks WHERE id = ? ", [
+      req.params.id,
+    ]);
 
-  if (result.length === 0)
-    return res.status(404).json({mensaje: "TASK NOT FOUNDp"})
+    if (result.length === 0)
+      return res.status(404).json({ mensaje: "TASK NOT FOUNDp" });
 
-  res.json(result[0]);
+    res.json(result[0]);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 //* ----------------- GET ALL TASKS -------------------------
 export const getTasks = async (req, res) => {
-  const [result] = await pool.query("SELECT * FROM tasks ORDER BY title ASC");
-  res.json(result);
+  try {
+    const [result] = await pool.query("SELECT * FROM tasks ORDER BY title ASC");
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 //* ----------------- CREATE TASK -------------------------
 export const createTask = async (req, res) => {
-  const { title, description } = req.body;
-  const [result] = await pool.query(
-    "INSERT INTO tasks (title, description) VALUES (? , ?)",
-    [title, description]
-  );
-  console.log(result);
-  res.json({
-    id: result.insertId,
-    title,
-    description,
-  });
-  res.send("creando Tareas");
+  try {
+    const { title, description } = req.body;
+    const [result] = await pool.query(
+      "INSERT INTO tasks (title, description) VALUES (? , ?)",
+      [title, description]
+    );
+    console.log(result);
+    res.json({
+      id: result.insertId,
+      title,
+      description,
+    });
+    res.send("creando Tareas");
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 //* ------------------ UPDATE TASK -------------------------
 export const updateTask = async (req, res) => {
-
-  const [result] = await pool.query("UPDATE tasks SET ? WHERE id = ? ", [
-    req.body,
-    req.params.id,
-  ]);
-  res.json(result);
-
+  try {
+    const [result] = await pool.query("UPDATE tasks SET ? WHERE id = ? ", [
+      req.body,
+      req.params.id,
+    ]);
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 //* ------------------ DELETE TASK -------------------------
 export const deleteTask = async (req, res) => {
+  try {
     const [result] = await pool.query("DELETE FROM tasks WHERE id = ? ", [
-    req.params.id,
-  ]);
+      req.params.id,
+    ]);
 
-  if (result.length === 0)
-    return res.status(404).json({mensaje: "TASK NOT FOUND"})
+    if (result.length === 0)
+      return res.status(404).json({ mensaje: "TASK NOT FOUND" });
 
-  return res.sendStatus(204)
+    return res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
