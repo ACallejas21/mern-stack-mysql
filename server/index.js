e103ab15bee6a64da9d2327508bf3d747d8e020b
Acalleas21 <akdiel.callejas21@gmail.com>
2023-03-08 14:26:08 -0600

    configuración para cors politic

--- server/index.js
+++ server/index.js
@@ -1,18 +1,19 @@
 import express from "express";
+import cors from "cors";
 import { PORT } from "./config.js";
 
 //*Rutas
 import indexRoutes from "./routes/index.routes.js";
 import taskRoutes from "./routes/task.routes.js";
 
+const app = express();
 
-const app = express()
+app.use(cors());
+app.use(express.json());
 
-app.use(express.json())
+app.use(indexRoutes);
+app.use(taskRoutes);
 
-app.use(indexRoutes)
-app.use(taskRoutes)
-
-app.listen(PORT)
+app.listen(PORT);
 
 console.log(`server listening on port ${PORT}`);
