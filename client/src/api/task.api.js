7cd33a142b8e753512076dddbdb9b62288c531b4
Acalleas21 <akdiel.callejas21@gmail.com>
2023-03-08 14:43:40 -0600

    ruta para tareas

--- client/src/api/task.api.js
+++ client/src/api/task.api.js
@@ -1,4 +1,11 @@
 import axios from "axios";
 
+
+//* -----------------GET TASK---------------------
+export const GetTaskRequest = async () =>
+  await axios.get("http://localhost:4000/tasks");
+
+
+//* -----------------CREATE TASK---------------------
 export const createTaskRequest = async (task) =>
   await axios.post("http://localhost:4000/tasks", task);
