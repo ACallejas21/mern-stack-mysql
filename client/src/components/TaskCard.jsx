e2dcc4753df9e2fe662a201b5f9ec95e1c50f4ff
Acalleas21 <akdiel.callejas21@gmail.com>
2023-03-13 14:47:44 -0500

    funcion delete task

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { useTask } from '../context/TaskProvider.jsx'
 
 function TaskCard({task}) {
+
+    const {DeleteTask} = useTask()
+
+
   return (
     <div>
         <h2>{task.title}</h2>
         <p>{task.description}</p>
         <span>{task.done == 1 ? '✅' : '❌'}</span>
         <span>{task.createAt}</span>
-        <button>DELETE</button>
+        <button onClick={() => DeleteTask(task.id)}>DELETE</button>
         <button>UPDATE</button>
       </div>
   )
