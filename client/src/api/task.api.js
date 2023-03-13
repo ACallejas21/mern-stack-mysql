5d0525d259fb9633e852ad1fead2262fb7bbe5a5
Acalleas21 <akdiel.callejas21@gmail.com>
2023-03-13 14:47:03 -0500

    rutaspara las consultas

--- client/src/api/task.api.js
+++ client/src/api/task.api.js
@@ -1,11 +1,16 @@
 import axios from "axios";
 
+const rootURL= 'http://localhost:4000';
 
 //* -----------------GET TASK---------------------
 export const GetTaskRequest = async () =>
-  await axios.get("http://localhost:4000/tasks");
+  await axios.get(`${rootURL}/tasks`);
 
 
 //* -----------------CREATE TASK---------------------
 export const createTaskRequest = async (task) =>
-  await axios.post("http://localhost:4000/tasks", task);
+  await axios.post(`${rootURL}/tasks`, task);
+
+  //* -----------------DELETE TASK---------------------
+export const deleteTaskRequest = async (id) =>
+await axios.delete(`${rootURL}/tasks/${id}`,);
