a25d73cef405aea11995c89c143484fdff080482
Acalleas21 <akdiel.callejas21@gmail.com>
2023-03-13 14:45:52 -0500

    agregando el contexto

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 
-
 import TaskForm from "./pages/TaskForm"
 import TaskPage from "./pages/TaskPage"
 import NotFound from "./pages/NotFound"
 
 import Navbar from "./components/Navbar";
 
+import { TaskContextProvider } from "./context/TaskProvider";
+
 function App() {
   return (
     <>
+    <TaskContextProvider>
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<TaskPage></TaskPage>}></Route>
         <Route path="/new" element={<TaskForm></TaskForm>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
+      </TaskContextProvider>
     </>
   )
 }
