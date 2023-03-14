9fb5b470447996585bf53597816a288fe53a4768
Acalleas21 <akdiel.callejas21@gmail.com>
2023-03-14 10:48:37 -0500

    update || toggle task

--- client/src/api/task.api.js
+++ client/src/api/task.api.js
@@ -2,9 +2,13 @@ import axios from "axios";
 
 const rootURL= 'http://localhost:4000';
 
-//* -----------------GET TASK---------------------
+//* -----------------GET TASKS---------------------
 export const GetTaskRequest = async () =>
   await axios.get(`${rootURL}/tasks`);
+
+    //* -----------------GET TASK---------------------
+export const GetOneTaskRequest = async (id) =>
+await axios.get(`${rootURL}/tasks/${id}`,);
 
 
 //* -----------------CREATE TASK---------------------
@@ -14,3 +18,12 @@ export const createTaskRequest = async (task) =>
   //* -----------------DELETE TASK---------------------
 export const deleteTaskRequest = async (id) =>
 await axios.delete(`${rootURL}/tasks/${id}`,);
+
+  //* -----------------UPDATE TASK---------------------
+  export const updateTaskRequest = async (id, newFields) =>
+  await axios.put(`${rootURL}/tasks/${id}`, newFields);
+
+  
+  //* -----------------Toggle TASK---------------------
+  export const toggleTaskRequest = async (id, done) =>
+  await axios.put(`${rootURL}/tasks/${id}`, {done});
